43dcdd95a9b1b3093c04fae811fc0eee793b8adf
stepmuel <stephan@heap.ch>
2018-05-18 17:20:40 +0200

    Change cors preflight response to allow any headers

--- src/app-factory.js
+++ src/app-factory.js
@@ -38,9 +38,11 @@ function init(config) {
     app.use((req, res, next) => {
       console.log(req.method + ' ' + req.url);
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Auth-Token, Content-Length, X-Requested-With');
       if (req.method == 'OPTIONS') {
+        // Preflight
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        const allowHeaders = req.header("Access-Control-Request-Headers") || 'Origin, X-Reqested-With, Accept';
+        res.header('Access-Control-Allow-Headers', allowHeaders);
         res.sendStatus(200);
       } else {
         next();
